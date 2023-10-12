8a51bee359e61210c47f4a45936f5f901324dc6c
hazadus <hazadus7@gmail.com>
2023-10-12 14:43:34 +0300

    fix: Solve problem with "Today" queryset setting date only on project start.

--- feeds/selectors.py
+++ feeds/selectors.py
@@ -4,27 +4,27 @@ from django.db.models import Count, QuerySet
 
 from feeds.models import Entry, Feed, Tag
 
-MODE_QUERYSETS = {
-    "all": Entry.objects.all(),
-    "today": Entry.objects.filter(
-        pub_date__day=datetime.today().day,
-        pub_date__month=datetime.today().month,
-        pub_date__year=datetime.today().year,
-    ),
-    "unread": Entry.objects.filter(is_read=False),
-    "read": Entry.objects.filter(is_read=True),
-    "favorites": Entry.objects.filter(is_favorite=True),
-}
-
 
 def get_entry_queryset(user, mode: str) -> QuerySet:
     """
     Return Entry queryset for "mode", where mode is one of "Smart Feed" names,
     i.e. all, today, unread, read, favorites.
     """
-    if mode not in MODE_QUERYSETS.keys():
-        raise KeyError(f"Incorrect mode '{mode}', must be in {MODE_QUERYSETS.keys()}")
-    return MODE_QUERYSETS[mode].filter(feed__user=user)
+    mode_querysets = {
+        "all": Entry.objects.all(),
+        "today": Entry.objects.filter(
+            pub_date__day=datetime.today().day,
+            pub_date__month=datetime.today().month,
+            pub_date__year=datetime.today().year,
+        ),
+        "unread": Entry.objects.filter(is_read=False),
+        "read": Entry.objects.filter(is_read=True),
+        "favorites": Entry.objects.filter(is_favorite=True),
+    }
+
+    if mode not in mode_querysets.keys():
+        raise KeyError(f"Incorrect mode '{mode}', must be in {mode_querysets.keys()}")
+    return mode_querysets[mode].filter(feed__user=user)
 
 
 def get_entry_count(user, mode: str) -> int:
