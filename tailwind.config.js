ef13115b40d4e05d47de6fda027f0f6513ed3e72
hazadus <hazadus7@gmail.com>
2023-10-27 11:15:26 +0300

    ui: Hide scrollbar in 1st column.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -8,5 +10,17 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/typography'),
+    // Reference: https://stackoverflow.com/questions/66416614/how-to-create-scrollable-element-in-tailwind-without-a-scrollbar
+    plugin(function({ addUtilities }) {
+      addUtilities({
+        '.hide-scrollbar::-webkit-scrollbar': {
+          'display': 'none',
+        },
+        '.hide-scrollbar': {
+          '-ms-overflow-style': 'none',
+          'scrollbar-width': 'none',
+        },
+      })
+    })
   ],
 }
