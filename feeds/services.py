da394722d0e255e317382bfc95932fbfab5f7714
hazadus <hazadus7@gmail.com>
2024-01-06 17:14:33 +0300

    refactor: Removed unused TODOs.

--- feeds/services.py
+++ feeds/services.py
@@ -312,9 +312,6 @@ def _entry_create_from_data_if_not_exists(feed: Feed, entry_data: dict) -> Entry
 
     :return: created `Entry` or `None`
     """
-    # TODO: split data parsing to separate function
-    # TODO: introduce new data type, e.g. EntryData (all stuff required to create Entry instance)
-    # TODO: introduce new data type, e.g. TagInfo (required to create Tag instance)
     entry_instance = None
     title = entry_data.get("title")
     link = entry_data.get("link")
